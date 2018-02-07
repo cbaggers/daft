8c924fe681cfe807fa09284eac0640351c8aa872
cbaggers <chris.bagley@gmail.com>
2018-02-07 21:45:19 +0100

    allow checking for collision with kind of actor

--- collision.lisp
+++ collision.lisp
@@ -1,13 +1,25 @@
 (in-package :daft)
 
-(defun touching-p (&optional set-of-actors)
-  (declare (ignore set-of-actors))
-  (let ((self *self*))
-    (loop :for actor :across *current-actors*
-       :when (and (not (eq (slot-value actor 'next)
-                           self))
-                  (%touching-p self actor))
-       :collect actor)))
+(defun touching-p (&optional set-of-actors/actor-kind)
+  (let ((self *self*)
+        (target (or set-of-actors/actor-kind
+                    *current-actors*)))
+    (if (symbolp target)
+        (%touching-kind-p self target)
+        (%touching-set-p self target))))
+
+(defun %touching-kind-p (self target)
+  (loop :for actor :across *current-actors*
+     :when (and (typep actor target)
+                (%touching-p self actor))
+     :collect actor))
+
+(defun %touching-set-p (self set)
+  (loop :for actor :across set
+     :when (and (not (eq (slot-value actor 'next)
+                         self))
+                (%touching-p self actor))
+     :collect actor))
 
 (defun %touching-p (a b)
   (let ((r-a (radius a))
