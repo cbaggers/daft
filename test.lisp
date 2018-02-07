72e5f9768994c88dab270406ebaaaf1d501aa9f9
cbaggers <chris.bagley@gmail.com>
2018-02-07 21:59:46 +0100

    add alien

--- test.lisp
+++ test.lisp
@@ -3,16 +3,27 @@
 (define-actor bullet ((:visual "bullet.png")
                       (speed 1))
   (:main
-   (when (or (touching-p 'ship) (> y 300))
+   (when (or (touching-p 'alien)
+             ;; TODO: WAT! ↓↓↓↓↓↓
+             (> (y (pos *self*)) 300))
      (die))
    (move-forward 2)))
+
+(define-actor alien ((:visual "alien.png")
+                     (health 10))
+  (:main
+   (strafe (* (sin (now)) 2))
+   (when (touching-p 'bullet)
+     (decf health)
+     (when (<= health 0)
+       (die)))))
 
 (define-actor ship ((:visual "shuttle2.png")
                     (start-time (now) t)
                     (fire (make-stepper (seconds 0.1)
                                         (seconds 0.1))))
   (:main
-   (incf x (* 2 (x (mouse-move (mouse)))))
+   (strafe (* 2 (x (mouse-move (mouse)))))
    (when (and (mouse-button (mouse) mouse.left)
               (funcall fire))
      (spawn 'bullet (v! 0 40)))))
