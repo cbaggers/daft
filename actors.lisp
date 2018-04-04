3849ccf61347ee5157dc8697bd931d09d3d474dd
cbaggers <chris.bagley@gmail.com>
2018-04-04 18:54:33 +0200

    turn the actors struct into a class while we are in heavy flux

--- actors.lisp
+++ actors.lisp
@@ -59,13 +59,31 @@
 
 ;;------------------------------------------------------------
 
-(defstruct actors
-  (current (make-array 0 :adjustable t :fill-pointer 0)
-           :type (array t (*)))
-  (next (make-array 0 :adjustable t :fill-pointer 0)
-        :type (array t (*)))
-  (collision-texture nil :type (or null texture))
-  (coll-sampler nil :type (or null sampler)))
+(defclass actors ()
+  ((current :initform (make-array 0 :adjustable t :fill-pointer 0)
+            :initarg :current
+            :type (array t (*))
+            :accessor actors-current)
+   (next :initform (make-array 0 :adjustable t :fill-pointer 0)
+         :initarg :next
+         :type (array t (*))
+         :accessor actors-next)
+   (collision-texture :initform nil
+                      :initarg :collision-texture
+                      :type (or null texture)
+                      :accessor actors-collision-texture)
+   (coll-sampler :initform nil
+                 :initarg :coll-sampler
+                 :type (or null sampler)
+                 :accessor actors-coll-sampler)))
+
+(defun make-actors (&key current next collision-texture coll-sampler)
+  (make-instance
+   'actors
+   :current (or current (make-array 0 :adjustable t :fill-pointer 0))
+   :next (or next (make-array 0 :adjustable t :fill-pointer 0))
+   :collision-texture collision-texture
+   :coll-sampler coll-sampler))
 
 (defun get-actor-arrays (type)
   (or (gethash type *actors*)
