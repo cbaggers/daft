e846daeb25a16daf8bd0850289af54482da8c802
cbaggers <chris.bagley@gmail.com>
2018-04-21 09:24:46 +0200

    woops, forgot exports :D

--- package.lisp
+++ package.lisp
@@ -1,9 +1,61 @@
 ;;;; package.lisp
 
 (uiop:define-package #:daft
-  (:use #:cl :cepl :vari :rtg-math :nineveh :dirt
-        :temporal-functions :cepl.skitter :with-setf
-        :cepl.defn)
+    (:use #:cl :cepl :vari :rtg-math :nineveh :dirt
+          :temporal-functions :cepl.skitter :with-setf
+          :cepl.defn)
   (:import-from :alexandria
                 :symbolicate
-                :with-gensyms))
+                :with-gensyms)
+  (:export
+   :actor
+   :actor-kind
+   :camera
+   :public-state
+   :scene
+   :as
+   :define-actor
+   :define-god
+   :define-scene
+   :*screen-height-in-game-units*
+   :advance-frame
+   :angle-between
+   :angle-to
+   :change-scene
+   :change-state
+   :coll-with
+   :compass-angle
+   :compass-dir
+   :compass-dir-move
+   :copy-actor-state
+   :daft
+   :die
+   :direction-to
+   :hey
+   :in-screen-p
+   :in-world-p
+   :is-alive
+   :is-dead
+   :make-camera
+   :move-away-from
+   :move-forward
+   :move-towards
+   :next-frame
+   :now
+   :pad-1d
+   :pad-button
+   :play-sound
+   :radius
+   :scene
+   :set-angle-from-analog
+   :spawn!
+   :strafe
+   :strafe-towards
+   :touching-p
+   :turn-left
+   :turn-right
+   :turn-towards
+   :*current-scene*
+   :*daft-frame-counter*
+   :*fps*
+   :*scenes*))
