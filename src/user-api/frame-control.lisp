defa545097f1d8f16956126937e93274a0f1f901
cbaggers <chris.bagley@gmail.com>
2018-04-25 22:12:35 +0200

    add last-frame next-frame & get-frame

--- src/user-api/frame-control.lisp
+++ src/user-api/frame-control.lisp
@@ -18,6 +18,22 @@
                         start-frame)
                      0f0))))))
 
+(defun+ last-frame (&optional range)
+  (declare (profile t))
+  (let ((start-frame (if range
+                         (first range)
+                         0))
+        (anim-length (if range
+                         (second range)
+                         (slot-value (kind *self*) 'anim-length))))
+    (with-slots (anim-frame) *self*
+      (let ((frame (- anim-frame start-frame)))
+        (setf anim-frame
+              (float (+ (mod (- (floor frame) 1)
+                             anim-length)
+                        start-frame)
+                     0f0))))))
+
 (defun+ advance-frame (amount &optional range)
   (declare (profile t))
   (let* ((start-frame (if range
@@ -33,5 +49,16 @@
                              anim-length)
                         start-frame)
                      0f0))))))
+
+(defun+ set-frame (frame)
+  (declare (profile t))
+  (let ((anim-length (slot-value (kind *self*) 'anim-length)))
+    (with-slots (anim-frame) *self*
+      (setf anim-frame
+            (float (max (min frame anim-length) 0)
+                   0f0)))))
+
+(defun get-frame ()
+  (slot-value *self* 'anim-frame))
 
 ;;------------------------------------------------------------
