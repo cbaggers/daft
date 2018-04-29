f759d0481bf55162032982f279b3c8e6d15e1e8f
cbaggers <chris.bagley@gmail.com>
2018-04-29 02:17:06 +0200

    warning on coll request against unknown actor kind

--- src/user-api/collision.lisp
+++ src/user-api/collision.lisp
@@ -17,9 +17,13 @@
   (declare (profile t))
   (with-slots (kind id) *self*
     (let ((target-kind (%get-actor-kind-by-name *current-scene* actor-kind)))
-      (setf (gethash target-kind (kinds-to-test-collision-with kind)) t)
-      (let ((results (gethash target-kind (collision-results kind))))
-        (when (and id results)
-          (> (aref-c results id) 0))))))
+      (if target-kind
+          (progn
+            (setf (gethash target-kind (kinds-to-test-collision-with kind)) t)
+            (let ((results (gethash target-kind (collision-results kind))))
+              (when (and id results)
+                (> (aref-c results id) 0))))
+          (warn "Cant check for collision with ~s.~%Not a known kind of actor"
+                actor-kind)))))
 
 ;;------------------------------------------------------------
